843054a58be27be72d1cdc8ea7a3f6b632edd8ec
ramarfx <ramagamiing33@gmail.com>
2024-08-19 00:28:03 +0700

    fix: vercel router error

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomeLayout />} /> 
+        <Route path="/" element={<HomeLayout />} />
         <Route path="/team" element={<Team />} />
         <Route path="/baby" element={<Baby />} />
       </Routes>
