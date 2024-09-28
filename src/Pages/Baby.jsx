bdb9cf70c97fc19ce417c27c243846aaa089618a
reynaldo0 <reynaldooyusellino@gmail.com>
2024-09-28 10:37:08 +0700

    upd : konten

--- src/Pages/Baby.jsx
+++ src/Pages/Baby.jsx
@@ -847,7 +847,7 @@ const Baby = () => {
                 data-aos-easing="ease-in-out"
                 data-aos-duration="700"
               >
-                Lorem Ipsum Dolor Sit Amet Labore
+                Gaya Hidup Sehat Bersama Kami!
               </h2>
               <p
                 className="text-lg text-white mb-8"
@@ -855,9 +855,7 @@ const Baby = () => {
                 data-aos-easing="ease-in-out"
                 data-aos-duration="800"
               >
-                Pastikan bayi mendapat nutrisi optimal dengan rencana ASI yang
-                tepat. Temukan tips untuk menjadikan proses menyusui lebih
-                lancar dan nyaman.
+                Kami memiliki layanan untuk berkonsultasi dengan kami, tim HealThyself akan selalu memberikan saran dan bimbingan kepadamu! Masukkan nama, email serta keluhan kamu!
               </p>
 
               {/* Form section */}
--- src/Pages/Teen.jsx
+++ src/Pages/Teen.jsx
@@ -808,7 +808,7 @@ const Teen = () => {
                 data-aos-easing="ease-in-out"
                 data-aos-duration="700"
               >
-                Lorem Ipsum Dolor Sit Amet Labore
+                Gaya Hidup Sehat Bersama Kami!
               </h2>
               <p
                 className="text-lg text-white mb-8"
@@ -816,9 +816,7 @@ const Teen = () => {
                 data-aos-easing="ease-in-out"
                 data-aos-duration="800"
               >
-                Pastikan bayi mendapat nutrisi optimal dengan rencana ASI yang
-                tepat. Temukan tips untuk menjadikan proses menyusui lebih
-                lancar dan nyaman.
+                Kami memiliki layanan untuk berkonsultasi dengan kami, tim HealThyself akan selalu memberikan saran dan bimbingan kepadamu! Masukkan nama, email serta keluhan kamu!
               </p>
 
               {/* Form section */}
