13675fe7f14a684555f366ea74441e10ceab8973
reynaldo0 <reynaldooyusellino@gmail.com>
2025-01-10 10:02:46 +0700

    fix : team

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -110,6 +110,50 @@ const Team = () => {
               </div>
             </div>
           </SwiperSlide>
+          <SwiperSlide>
+            <div className="group transition-all bg-white p-4 rounded-xl shadow border border-gray-200 text-center ease-in-out hover:border-2 hover:border-secondary hover:bg-secondary hover:text-white flex flex-col items-center h-full">
+              <img
+                className="w-52 h-52 mx-auto rounded-full object-cover border-4 border-gray-200 ease-in-out group-hover:border-white"
+                src="/team/nazla.jpg"
+                alt="Nazla Rahma"
+              />
+              <h3 className="text-xl font-semibold mt-4 ease-in-out group-hover:text-lg">Nazla Rahma</h3>
+              <p className="text-gray-600 text-sm ease-in-out group-hover:text-white">UI/UX, Illustrator</p>
+              <div className="flex justify-center space-x-4 mt-4">
+                <a href="https://github.com/zlaraa" className="text-secondary ease-in-out group-hover:text-white text-4xl p-5">
+                  <FontAwesomeIcon icon={faGithub}/>
+                </a>
+                <a href="https://www.linkedin.com/in/nazla-rahma" className="text-secondary ease-in-out group-hover:text-white text-4xl p-5">
+                  <FontAwesomeIcon icon={faLinkedin}/>
+                </a>
+                <a href="https://www.instagram.com/nazlarhm96" className="text-secondary ease-in-out group-hover:text-white text-4xl p-5">
+                  <FontAwesomeIcon icon={faInstagram}/>
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="group transition-all bg-white p-4 rounded-xl shadow border border-gray-200 text-center ease-in-out hover:border-2 hover:border-secondary hover:bg-secondary hover:text-white flex flex-col items-center h-full">
+              <img
+                className="w-52 h-52 mx-auto rounded-full object-cover border-4 border-gray-200 ease-in-out group-hover:border-white"
+                src="/team/akmal.jpg"
+                alt="Muhammad Akmal Saban"
+              />
+              <h3 className="text-xl font-semibold mt-4 ease-in-out group-hover:text-lg">Muhammad Akmal Saban</h3>
+              <p className="text-gray-600 text-sm ease-in-out group-hover:text-white">Progammer</p>
+              <div className="flex justify-center space-x-4 mt-4">
+                <a href="https://github.com/AkmaldanKamu" className="text-secondary ease-in-out group-hover:text-white text-4xl p-5">
+                  <FontAwesomeIcon icon={faGithub}/>
+                </a>
+                <a href="https://id.linkedin.com/in/ramadina-al-muzthazam-5028482a2" className="text-secondary ease-in-out group-hover:text-white text-4xl p-5">
+                  <FontAwesomeIcon icon={faLinkedin}/>
+                </a>
+                <a href="https://www.instagram.com/m.akmal.saban" className="text-secondary ease-in-out group-hover:text-white text-4xl p-5">
+                  <FontAwesomeIcon icon={faInstagram}/>
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
         </Swiper>
       </div>
     </section>
